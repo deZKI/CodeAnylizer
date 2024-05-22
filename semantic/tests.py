216f3f05324b4f782d58ca47d9c695d402b7cdee
kiril <kirill.301522@gmail.com>
2024-05-23 00:47:21 +1000

    semantic analyzer

--- semantic/tests.py
+++ semantic/tests.py
@@ -55,7 +55,7 @@ class TestSemanticAnalyzer(unittest.TestCase):
         semantic_analyzer = SemanticAnalyzer(ast)
         with self.assertRaises(Exception) as cm:
             semantic_analyzer.analyze()
-        self.assertEqual(str(cm.exception), 'Semantic Error: variable y not declared on line 1')
+        self.assertEqual(str(cm.exception), 'Semantic Error: variable y not declared on line 3')
 
     def test_redeclaration_of_variable(self):
         """
